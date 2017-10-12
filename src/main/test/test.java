688c8f4474b0249462f45b6e9c7dc26a52448ce7
zouhaiming2017 <506245357@qq.com>
2017-10-12 09:23:54 +0800

    mybatis-plus

---
+++ src/main/test/test.java
@@ -0,0 +1,31 @@
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class test {
+	public static void method(Object obj, String filed) {
+		try {
+			Class clazz = obj.getClass();
+			PropertyDescriptor pd = new PropertyDescriptor(filed, clazz);
+			Method getMethod = pd.getReadMethod();//获得get方法
+
+			if (pd != null) {
+
+				Object o = getMethod.invoke(obj);//执行get方法返回一个Object
+				System.out.println(o);
+
+			}
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IntrospectionException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+}
